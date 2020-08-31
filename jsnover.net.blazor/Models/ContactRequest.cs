9dd5b9ac84c19a4c068e5bb054b5a26170bc1ae6
Jacob R. Snover <snoverjacob@yahoo.com>
2020-08-31 12:30:01 -0400

    initial commit

---
+++ jsnover.net.blazor/Models/ContactRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jsnover.net.blazor.Models
+{
+    public partial class ContactRequest
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string UserName { get; set; }
+        public bool Issue { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
