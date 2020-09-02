eab733244ac9bd2ecd21c76e6f50280f84e80c1d
Jacob R. Snover <snoverjacob@yahoo.com>
2020-09-02 09:54:24 -0400

    contact page and email service update

---
+++ jsnover.net.blazor/DataTransferObjects/Common/ContactModel.cs
@@ -0,0 +1,39 @@
+﻿using jsnover.net.blazor.DataTransferObjects.Interfaces;
+using jsnover.net.blazor.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jsnover.net.blazor.DataTransferObjects.Common
+{
+    public class ContactModel : IDtoMapper<ContactModel, ContactRequest>
+    {
+        [StringLength(maximumLength: 50, ErrorMessage = "Must be under 50 characters")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(maximumLength: 256, ErrorMessage = "Email Address is too long.")]
+        public string Email { get; set; }
+
+        [StringLength(maximumLength: 500, ErrorMessage = "Please keep comment under 500 Characters")]
+        public string Body { get; set; }
+
+        public bool Issue { get; set; } = false;
+
+        public static ContactRequest MapToDto(ContactModel comment)
+        {
+            return new ContactRequest
+            {
+                Id = 0,
+                DatePosted = DateTime.Now.Date,
+                Email = comment.Email,
+                UserName = comment.Name ?? null,
+                Body = comment.Body ?? string.Empty,
+                Issue = comment.Issue
+            };
+        }
+    }
+}
---
+++ jsnover.net.blazor/Infrastructure/Services/EmailService.cs
@@ -0,0 +1,67 @@
+﻿using jsnover.net.blazor.DataTransferObjects.Common;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Threading.Tasks;
+
+namespace jsnover.net.blazor.Infrastructure.Services
+{
+    public class EmailService
+    {        
+        internal static async Task NotifySnover(ContactModel contactRequest)
+        {
+            var messageToSend = new MimeMessage
+            {
+                Sender = new MailboxAddress("jsnover.net", "fourseasonflora@gmail.com"),
+                Subject = "jsnover.net New Contact Request",
+            };
+
+            messageToSend.Body = new TextPart(TextFormat.Html)
+            {
+                Text =
+                $"<strong>NEW CONTACT REQUEST</strong><br/>" +
+                $"<strong>Name</strong>: {contactRequest.Name}" +
+                $"<strong>Email</strong>: {contactRequest.Email}<br/>" +
+                $"<strong>Request Body</strong>: {contactRequest.Body}" +
+                $"<strong>ISSUE WITH SITE</strong>: {contactRequest.Issue}<br/>"
+            };
+
+            messageToSend.To.Add(new MailboxAddress("Randy", "snoverjacob@yahoo.com"));
+
+            await SendEmail(messageToSend);
+        }
+
+        private static async Task SendEmail(MimeMessage messageToSend)
+        {
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                smtp.MessageSent += (sender, args) => { };
+                smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+                var email = Environment.GetEnvironmentVariable("Flora:Email", EnvironmentVariableTarget.Process);
+                var password = Environment.GetEnvironmentVariable("Flora:Password", EnvironmentVariableTarget.Process);
+
+                if (email is null)
+                {
+                    email = builder.GetValue<string>("Flora:Email");
+                }
+                if (password is null)
+                {
+                    password = builder.GetValue<string>("Flora:Password");
+                }
+
+
+                await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(email, password);
+                await smtp.SendAsync(messageToSend);
+                await smtp.DisconnectAsync(true);
+            }
+        }
+    }
+}
