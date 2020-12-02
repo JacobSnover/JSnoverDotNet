6b583283aaa8ada5334b9234bc8f42c4bd0e70be
Jacob Snover <snoverjacob@yahoo.com>
2020-12-02 02:06:05 -0500

    UI updates, blog search logic started, search UI implemented

--- jsnover.net/jsnover.net.blazor/Constants/Keywords.cs
+++ jsnover.net/jsnover.net.blazor/Constants/Keywords.cs
@@ -3,5 +3,12 @@
     public class Keywords
     {
         public const string Remove = "Remove";
+        public const string Title = "Title";
+        public const string Topic = "Topic";
+        public const string Tags = "Tags";
+        public const string Section = "Section";
+        public const string ListName = "List`1";
+
+        public const int ZeroIndex = 0;
     }
 }
