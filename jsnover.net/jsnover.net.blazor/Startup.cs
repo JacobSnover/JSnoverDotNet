c1be92de4ec6d01b49614ad6220ebcb1f21e033d
fourseasonflora@gmail.com <fourseasonflora@gmail.com>
2022-08-23 19:36:59 -0400

    Add link to solitaire page, added http client to services, connected to Api, added new deck and draw card

--- jsnover.net/jsnover.net.blazor/Startup.cs
+++ jsnover.net/jsnover.net.blazor/Startup.cs
@@ -52,6 +52,8 @@ namespace jsnover.net.blazor
             services.AddScoped<BlogService>();
             services.AddScoped<BlogViewModel>();
             services.AddBlazoredSessionStorage();
+            services.AddControllersWithViews();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
