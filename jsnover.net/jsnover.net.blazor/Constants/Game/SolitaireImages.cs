a644a4e18bae2bb2fe077c5567c9feae015d1345
Jacob Snover <snoverjacob@yahoo.com>
2022-08-29 19:53:29 -0400

    add cards to the front end, added hard coded values for some cards, and css isolation

---
+++ jsnover.net/jsnover.net.blazor/Constants/Game/SolitaireImages.cs
@@ -0,0 +1,11 @@
+﻿namespace jsnover.net.blazor.Constants.Game
+{
+    public class SolitaireImages
+    {
+        public const string BacokOfCardImage = "http://deckofcardsapi.com/static/img/back.png";
+        public const string AceHearts = "http://deckofcardsapi.com/static/img/AH.png";
+        public const string AceSpades = "http://deckofcardsapi.com/static/img/AS.png";
+        public const string AceClubs = "http://deckofcardsapi.com/static/img/AC.png";
+        public const string AceDiamonds = "http://deckofcardsapi.com/static/img/AD.png";
+    }
+}
